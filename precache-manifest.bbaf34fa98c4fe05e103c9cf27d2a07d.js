8400c9d54d901bb760890e1a9c0da038cd55aa2e
Kopal Mehta <kopalmehta4@gmail.com>
2020-04-02 00:29:20 +0530

    Updates

---
+++ precache-manifest.bbaf34fa98c4fe05e103c9cf27d2a07d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f08ea9af8656a0c5581",
+    "url": "/km-js/fcc-random-quote-generator.git/static/js/runtime~main.ae89fa6f.js"
+  },
+  {
+    "revision": "35212e375626ae984149",
+    "url": "/km-js/fcc-random-quote-generator.git/static/js/main.ed76438c.chunk.js"
+  },
+  {
+    "revision": "4479f21578fe3d96bb3e",
+    "url": "/km-js/fcc-random-quote-generator.git/static/js/2.8edb5f72.chunk.js"
+  },
+  {
+    "revision": "35212e375626ae984149",
+    "url": "/km-js/fcc-random-quote-generator.git/static/css/main.2d9aeb33.chunk.css"
+  },
+  {
+    "revision": "862a89b1e1af6165b377f046a9029988",
+    "url": "/km-js/fcc-random-quote-generator.git/index.html"
+  }
+];
